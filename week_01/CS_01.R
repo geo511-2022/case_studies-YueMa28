af838e6e05b232f8c1d8ea374206c2d90164b602
YueMa28 <mayue1240@gmail.com>
2022-09-27 19:09:20 -0400

    code for case study 01

---
+++ week_01/CS_01.R
@@ -0,0 +1,23 @@
+
+# load the iris dataset
+data(iris)
+
+# explore the mean function
+?mean
+
+# calculate the mean of the petal length
+len = iris$Petal.Length
+petal_length_mean = mean(len)
+
+
+#plot using hist() and save it
+png(file="/Users/mayue/Downloads/GEO511/CS_01/petal_length_distribution_hist.png",
+    width=500, height= 400)
+hist(len,
+     col="purple", border="white",
+     main="Distribution of the Petal Length",
+     xlab="Petal Length",
+     ylab="Count")
+dev.off()
+
+
