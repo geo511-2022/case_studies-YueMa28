968912df74e06c67c6218558c3a81ee782501ca4
YueMa28 <yma28@buffalo.edu>
2022-09-27 14:52:49 -0400

    test

---
+++ week_03/CS_04.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+library(nycflights13)
+
+farthest_distance_df <- flights %>%
+  group_by(origin) %>%
+  summarise(distance=max(distance))
+
+result <- inner_join(flights,farthest_distance_df,by=c("origin","distance"))
+
+result
+
+result_new <- select(result,c("origin","distance","dest"))
+
+result_new <- unique(result_new)
+
+result_new2 <- inner_join(result_new,airports,by=c("dest"="faa")) %>% select("origin","distance","name")
+
+result_new2
+
+install.packages("usethis")
+library(usethis)
+use_git_config(user.name = "YueMa28", user.email = "yma28@buffalo.edu")
