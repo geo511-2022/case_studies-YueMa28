195fcf70dbc45a98b2795b33088bd81ac0838b35
YueMa28 <109102864+YueMa28@users.noreply.github.com>
2022-09-27 14:37:18 -0400

    Update CS_02.R

--- week_02/CS_02.R
+++ week_02/CS_02.R
@@ -28,6 +28,6 @@ myimg <- ggplot(temp,aes(x=YEAR,y=JJA)) +
 
 myimg
 
-png(filename = "/Users/mayue/Downloads/GEO511/CS02/Mean_Summer_Temperatures_in_Buffalo.png",width=500,height=300)
+png(filename = "Mean_Summer_Temperatures_in_Buffalo.png",width=500,height=300)
 print(myimg)
 dev.off()
