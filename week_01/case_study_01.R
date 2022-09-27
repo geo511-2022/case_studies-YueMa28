0bcb9ba751ee17fea4b0c7ed181faf22e8243b1f
YueMa28 <109102864+YueMa28@users.noreply.github.com>
2022-09-27 14:40:30 -0400

    Update case_study_01.R

--- week_01/case_study_01.R
+++ week_01/case_study_01.R
@@ -1,2 +1,22 @@
+
+
+# load the iris dataset
 data(iris)
 
+# explore the mean function
+?mean
+
+# calculate the mean of the petal length
+len = iris$Petal.Length
+petal_length_mean = mean(len)
+
+
+#plot using hist() and save it
+png(file="petal_length_distribution_hist.png",
+    width=500, height= 400)
+hist(len,
+     col="purple", border="white",
+     main="Distribution of the Petal Length",
+     xlab="Petal Length",
+     ylab="Count")
+dev.off()
