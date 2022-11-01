c6a7b34fe69d578a72bfdea69a2f248d2b6741b6
YueMa28 <mayue1240@gmail.com>
2022-11-01 14:42:12 -0400

    code for case study 07

---
+++ week_07/CS_07.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(reprex)
+library(sf)
+library(ggplot2)
+library(spData)
+data(world)
+
+ggplot(world,aes(x=gdpPercap, y=continent, color=continent))+
+  geom_density(alpha=0.5,color=F)
+
+
+reprex(venue="gh")
+
+
+
